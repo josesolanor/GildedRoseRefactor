1dd8339f97d9d9759b67a2d7d358c5a2cf1caea1
Jose Solano R <josesolano@outlook.com>
2019-06-15 21:57:23 -0400

    Separando los metodos y creacion de constructor

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -7,65 +7,56 @@ namespace csharp
     {
         IList<Item> Items;
         IList<string> ItemsThatDoesntGetOlder;
+        IList<string> ItemsThatIncreaseQuality;
 
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
+            ItemsThatIncreaseQuality = new List<string>
+            {
+                "Aged Brie",
+                "Backstage passes to a TAFKAL80ETC concert"
+            };
             ItemsThatDoesntGetOlder = new List<string>
             {
                 "Sulfuras, Hand of Ragnaros"
             };
         }
-        private Boolean isUnderHighestQualityValue(Item item)
+        private Boolean IsUnderHighestQualityValue(Item item)
         {
             return item.Quality < 50;
         }
-        private Boolean isAboveLowestQualityValue(Item item)
+        private Boolean IsAboveLowestQualityValue(Item item)
         {
             return item.Quality > 0;
         }
         private void IncreaseQualityValue(Item item)
         {
-            if (isUnderHighestQualityValue(item))
+            if (IsUnderHighestQualityValue(item))
             {
-                item.Quality = item.Quality + 1;
+                item.Quality += GetBaseQuality(item);
             }
         }
         private void DecreaseQualityValue(Item item)
         {
-            if (isAboveLowestQualityValue(item))
+            if (IsAboveLowestQualityValue(item))
             {
-                item.Quality = item.Quality - 1;
+                item.Quality -= GetBaseQuality(item);
             }
+        }
+
+        private int GetBaseQuality(Item item)
+        {
+            return (item.SellIn >= 0) ? 1 : 2;
         }
 
         private void NormalUpdate(Item item)
         {
             DecreaseQualityValue(item);
-            if (item.SellIn < 0)
-            {
-                DecreaseQualityValue(item);
-            }
-        }
-
-        private void BrieUpdate(Item item)
-        {
-            IncreaseQualityValue(item);
-            if (item.SellIn < 0)
-            {
-                IncreaseQualityValue(item);
-            }
-        }
-
-        private void SulfurasUpdate(Item item)
-        {
-
         }
 
         private void BackstageUpdate(Item item)
         {
-            IncreaseQualityValue(item);
-
             if (item.SellIn < 10)
             {
                 IncreaseQualityValue(item);
@@ -82,27 +73,27 @@ namespace csharp
             }
         }
 
+        public void IncreasingItemsUpdate(Item item)
+        {
+            IncreaseQualityValue(item);
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                BackstageUpdate(item);
+            }
+        }
+
         public void UpdateQuality()
         {
             foreach (Item item in Items)
             {
-
                 if (!ItemsThatDoesntGetOlder.Contains(item.Name))
                 {
                     item.SellIn = item.SellIn - 1;
                 }
 
-                if (item.Name == "Sulfuras, Hand of Ragnaros")
+                if (ItemsThatIncreaseQuality.Contains(item.Name))
                 {
-                    SulfurasUpdate(item);
-                }
-                else if (item.Name == "Aged Brie")
-                {
-                    BrieUpdate(item);
-                }
-                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    BackstageUpdate(item);
+                    IncreasingItemsUpdate(item);
                 }
                 else
                 {
