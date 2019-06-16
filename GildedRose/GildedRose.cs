c92b0281526325bbca72240baeaf5e9007ee1a0c
Jose Solano R <josesolano@outlook.com>
2019-06-15 21:55:55 -0400

    añadiendo los cambios de metodos y juntandolos

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -6,35 +6,79 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
+        IList<string> ItemsThatDoesntGetOlder;
 
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
+            ItemsThatDoesntGetOlder = new List<string>
+            {
+                "Sulfuras, Hand of Ragnaros"
+            };
         }
-
-        private Boolean IsUnderHighestQualityValue(Item item)
+        private Boolean isUnderHighestQualityValue(Item item)
         {
             return item.Quality < 50;
         }
-
-        private Boolean IsAboveLowestQualityValue(Item item)
+        private Boolean isAboveLowestQualityValue(Item item)
         {
             return item.Quality > 0;
         }
-
         private void IncreaseQualityValue(Item item)
         {
-            if (IsUnderHighestQualityValue(item))
+            if (isUnderHighestQualityValue(item))
             {
                 item.Quality = item.Quality + 1;
             }
         }
-
         private void DecreaseQualityValue(Item item)
         {
-            if (IsAboveLowestQualityValue(item))
+            if (isAboveLowestQualityValue(item))
             {
                 item.Quality = item.Quality - 1;
+            }
+        }
+
+        private void NormalUpdate(Item item)
+        {
+            DecreaseQualityValue(item);
+            if (item.SellIn < 0)
+            {
+                DecreaseQualityValue(item);
+            }
+        }
+
+        private void BrieUpdate(Item item)
+        {
+            IncreaseQualityValue(item);
+            if (item.SellIn < 0)
+            {
+                IncreaseQualityValue(item);
+            }
+        }
+
+        private void SulfurasUpdate(Item item)
+        {
+
+        }
+
+        private void BackstageUpdate(Item item)
+        {
+            IncreaseQualityValue(item);
+
+            if (item.SellIn < 10)
+            {
+                IncreaseQualityValue(item);
+            }
+
+            if (item.SellIn < 5)
+            {
+                IncreaseQualityValue(item);
+            }
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
             }
         }
 
@@ -42,46 +86,27 @@ namespace csharp
         {
             foreach (Item item in Items)
             {
+
+                if (!ItemsThatDoesntGetOlder.Contains(item.Name))
+                {
+                    item.SellIn = item.SellIn - 1;
+                }
+
                 if (item.Name == "Sulfuras, Hand of Ragnaros")
                 {
-                    continue;
+                    SulfurasUpdate(item);
                 }
-                item.Quality = item.Quality - 1;
-                if (item.Name == "Aged Brie")
+                else if (item.Name == "Aged Brie")
                 {
-                    IncreaseQualityValue(item);
-                    if (item.SellIn < 0)
-                    {
-                        IncreaseQualityValue(item);
-                    }
+                    BrieUpdate(item);
                 }
                 else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    IncreaseQualityValue(item);
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 10)
-                        {
-                            IncreaseQualityValue(item);
-                        }
-                        if (item.SellIn < 5)
-                        {
-                            IncreaseQualityValue(item);
-                        }
-                        if (item.SellIn < 0)
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
+                    BackstageUpdate(item);
                 }
                 else
                 {
-                    DecreaseQualityValue(item);
-                    if (item.SellIn < 0)
-                    {
-                        DecreaseQualityValue(item);
-                    }
+                    NormalUpdate(item);
                 }
             }
         }
